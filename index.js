534845179d9a6297e1e362508d3db2bde6ebfd87
Basavaraj Halasur <basu@realtyxplore.com>
2017-06-24 13:53:41 +0530

    Initial scaffold

---
+++ index.js
@@ -0,0 +1,15 @@
+var firebase = require('firebase');
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyBED000V0fvARoL4wOGnrweO-BR7YmFYCo",
+    authDomain: "aiobee-b1390.firebaseapp.com",
+    databaseURL: "https://aiobee-b1390.firebaseio.com",
+    projectId: "aiobee-b1390",
+    storageBucket: "",
+    messagingSenderId: "104208339689"
+  };
+  firebase.initializeApp(config);
+
+  
